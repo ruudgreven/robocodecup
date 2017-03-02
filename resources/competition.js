ac283d5ee1377814cd544ee7f099fdbb0e261345
Timothy Sealy <timothy.sealy@gmail.com>
2017-03-02 14:48:34 +0100

    Wrapped array of competitions in a object with element 'competitions'.

--- resources/competition.js
+++ resources/competition.js
@@ -4,8 +4,105 @@ var router = express.Router();
 var Competition = require('../model/Competition');
 var Team = require('../model/Team');
 
-// List all competitions.
+/**
+ * @api {get} /api/competition List all competitions.
+ * @apiGroup Competitions
+ * @apiSuccess {Number} id Task id
+ * @apiSuccess {String} title Task title
+ * @apiSuccess {Boolean} done Task is done?
+ * @apiSuccess {Date} updated_at Update's date
+ * @apiSuccess {Date} created_at Register's date
+ * @apiSuccessExample {json} Success
+ *    HTTP/1.1 200 OK
+ *    {
+ *      "id": 1,
+ *      "title": "Study",
+ *      "done": false
+ *      "updated_at": "2016-02-10T15:46:51.778Z",
+ *      "created_at": "2016-02-10T15:46:51.778Z"
+ *    }
+ * @apiErrorExample {json} Find error
+ *    HTTP/1.1 500 Internal Server Error
+ */
 router.get('/', function (req, res) {
+
+    var where = {};
+    if (req.query.featured !== undefined){
+        where = {featured: req.query.featured};
+    }
+
+    var fields = {}
+    Competition.find(where, {}, function (err, competitions) {
+        if (err) {
+            console.error(err);
+            res.status(500).json({error: "true", message: "Cannot find competitions"});
+        }
+        var results = { competitions: competitions };
+        res.status(200).json(results);
+    });
+
+});
+
+/**
+ * @api {get} /api/competition/<competition_code> List details of a competition.
+ * @apiGroup Competitions
+ * @apiSuccess {Number} id Task id
+ * @apiSuccess {String} title Task title
+ * @apiSuccess {Boolean} done Task is done?
+ * @apiSuccess {Date} updated_at Update's date
+ * @apiSuccess {Date} created_at Register's date
+ * @apiSuccessExample {json} Success
+ *    HTTP/1.1 200 OK
+ *    {
+ *      "id": 1,
+ *      "title": "Study",
+ *      "done": false
+ *      "updated_at": "2016-02-10T15:46:51.778Z",
+ *      "created_at": "2016-02-10T15:46:51.778Z"
+ *    }
+ * @apiErrorExample {json} Find error
+ *    HTTP/1.1 500 Internal Server Error
+ */
+router.get('/:competition_code', function (req, res) {
+
+    Competition.findOne({code: req.params.competition_code}, {},  function (err, competitions) {
+        if (err) {
+            console.error(err);
+            res.status(500).json({error: "true", message: "Cannot find competitions"});
+        }
+        if (competitions === null || competitions.length == 0) {
+            res.status(404).json();
+        } else {
+            res.status(200).json(competitions);
+        }
+    });
+
+});
+
+/**
+ * @api {get} /api/competition/<competition_code> Details competition.
+ * @apiGroup Competitions
+ * @apiSuccess {Number} id Task id
+ * @apiSuccess {String} title Task title
+ * @apiSuccess {Boolean} done Task is done?
+ * @apiSuccess {Date} updated_at Update's date
+ * @apiSuccess {Date} created_at Register's date
+ * @apiSuccessExample {json} Success
+ *    HTTP/1.1 200 OK
+ *    {
+ *      "code": "useb_2017",
+ *      "name": "USEB 2017",
+ *      "description": "The Ultimate SElection Battle 2017",
+ *      "current_round: 2,
+ *      "rounds": [1,2],
+ *      "official": true,
+ *      "featured" : false
+ *    }
+ *
+ * @apiErrorExample {json} Find error
+ *    HTTP/1.1 500 Internal Server Error
+ */
+router.get('/:competition_code/round', function (req, res) {
 
     var where = {};
     if (req.query.featured !== undefined){
@@ -22,22 +119,6 @@ router.get('/', function (req, res) {
 
 });
 
-// List a specific CompetitionSrv.js.
-router.get('/:code', function (req, res) {
-
-    Competition.findOne({'code': req.params.code}, {},  function (err, competitions) {
-        if (err) {
-            console.error(err);
-            res.status(500).json({error: "true", message: "Cannot find competitions"});
-        }
-        if (competitions === null || competitions.length == 0) {
-            res.status(404).json();
-        } else {
-            res.status(200).json(competitions);
-        }
-    });
-
-});
 
 // List all teams in a specific CompetitionSrv.js.
 router.get('/:code/team', function (req, res) {
